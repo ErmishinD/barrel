cc8bf4ff3059e2cee1f06bba12b512cc80d22387
ErmishinD <ermishin.d.e@gmail.com>
2022-12-18 23:45:18 +0200

    npm build for admin pannel

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
             input: [
                 'resources/assets/scss/main.scss',
                 'resources/assets/js/main.js',
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
